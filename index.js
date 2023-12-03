5528387c08df1570282bb89080419e721405417d
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2023-12-03 11:04:29 +0530

    added scripts with nodemon

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const server = http.createServer((req, res) => {
   // request that in coming to the server
   // res
   // response that is out going from the server
-  res.writeHead(200, { batch: "B49WEENG" });
-  res.end("<h1>Hello world</h1>");
+  // res.writeHead(200, { batch: "B49WEENG" });
+  res.writeHead(200, { batch: "B49WEENG", "Content-Type": "application/json" });
+  // res.end("<h1>Hello world</h1>");
+  res.end(JSON.stringify({ msg: "Hello World" }));
 });
 
 server.listen(8000, () => {
